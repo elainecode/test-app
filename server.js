7022681855e2e49e228397da8fa04edf804fda20
elaine <elaine-me@elaines-MacBook-Pro.local>
2019-03-28 16:04:27 +0000

    path express path

--- server.js
+++ server.js
@@ -112,10 +112,10 @@ app.get('/api/v1/tags', async (req, res, next) => {
 });
 
   if (process.env.NODE_ENV === 'production') {
+    const path = require('path')
      app.use(express.static(path.join(__dirname, 'client/build')));
-     const path = require('path')
      app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
