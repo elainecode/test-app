103e0795a7303b643b499a82c855e507a2704ff2
elaine <elaine-me@elaines-mbp.attlocal.net>
2019-05-02 23:29:10 -0700

    added button to remove filter tags

--- client/src/components/ScrollSearch.js
+++ client/src/components/ScrollSearch.js
@@ -3,15 +3,26 @@ import './css/homeview.css'
 
 
 class ScrollSearch extends Component {
+    state = {
+      filterByTag: false
+    }
+     handleClick = (e) => {
+      e.preventDefault()
+      this.setState({filterByTag: !this.state.filterByTag})
+      this.props.tagOnlySearch(e)
+    }
   render() {
     const { tags, tagOnlySearch} = this.props
+    const { filterByTag } = this.state
     return (
        <div class="column is-1 is-offset-1">
         <ul id="tag-search">
-          
-         { tags.map( t => { 
+         { 
+          filterByTag
+      ? <input type='submit' value='Remove Filter' onClick={this.handleClick}/>
+      :  tags.map( t => { 
           return (
-          <li onClick={tagOnlySearch}>
+          <li onClick={this.handleClick}>
           <span id="search-tag">
           {t.tagname}
            </span>
